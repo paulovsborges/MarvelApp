f9ec624f4b06abd517411c78f6a1b0e97d61d4d1
Borges <paulo.borges@premiersoft.net>
2022-02-04 09:27:12 -0300

    configuring gradle

--- app/src/main/java/com/pvsb/marvelapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/pvsb/marvelapp/presentation/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pvsb.marvelapp
+package com.pvsb.marvelapp.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pvsb.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
