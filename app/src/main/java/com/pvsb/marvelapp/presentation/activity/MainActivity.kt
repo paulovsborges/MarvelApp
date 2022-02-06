ab3a5a7661ac7103a13dd24ee0731729ba133b50
Borges <paulo.borges@premiersoft.net>
2022-02-06 14:30:45 -0300

    initial config

--- app/src/main/java/com/pvsb/marvelapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/pvsb/marvelapp/presentation/activity/MainActivity.kt
@@ -2,11 +2,32 @@ package com.pvsb.marvelapp.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.pvsb.marvelapp.R
+import com.pvsb.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appbarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(
+            androidx.navigation.fragment.R.id.nav_host_fragment_container
+        ) as NavHostFragment
+
+        navController = navHost.navController
+        appbarConfiguration = AppBarConfiguration(
+            setOf(
+
+            )
+        )
     }
 }
