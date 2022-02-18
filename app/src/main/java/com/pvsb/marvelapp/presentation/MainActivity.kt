630a4ac9b0523da4ded3cb5f86c3fb391d220fc7
Borges <paulo.borges@premiersoft.net>
2022-02-18 16:51:29 -0300

    adding the new android 12 splash screen

--- app/src/main/java/com/pvsb/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/pvsb/marvelapp/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pvsb.marvelapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.pvsb.marvelapp.R
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
