17d2ea6a2dd9fe06bbfa72601a12e4262ab239fe
Borges <paulo.borges@premiersoft.net>
2022-02-23 09:09:59 -0300

    adding proguard rules

--- app/src/main/java/com/pvsb/marvelapp/framework/network/model/CharactersResponse.kt
+++ app/src/main/java/com/pvsb/marvelapp/framework/network/model/CharactersResponse.kt
@@ -1,29 +1,47 @@
 package com.pvsb.marvelapp.framework.network.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DataWrapperResponse(
+    @SerializedName("code")
     val code: Int,
+    @SerializedName("status")
     val status: String,
+    @SerializedName("copyright")
     val copyright: String,
+    @SerializedName("attributionText")
     val attributionText: String,
+    @SerializedName("attributionHTML")
     val attributionHTML: String,
+    @SerializedName("data")
     val data: ContainerResponse
 )
 
 data class ContainerResponse(
+    @SerializedName("offset")
     val offset: Int,
+    @SerializedName("limit")
     val limit: Int,
+    @SerializedName("total")
     val total: Int,
+    @SerializedName("count")
     val count: Int,
+    @SerializedName("results")
     val results: List<CharactersResponse>
 )
 
 data class CharactersResponse(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("thumbnail")
     val thumbnail: ImagesResponse
 )
 
 data class ImagesResponse(
+    @SerializedName("path")
     val path: String,
+    @SerializedName("extension")
     val extension: String
 )
