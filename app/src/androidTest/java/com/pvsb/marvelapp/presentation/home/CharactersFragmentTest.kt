46ac3b478b252265147d87168f5b16bb0322da8e
Borges <paulo.borges@premiersoft.net>
2022-04-08 12:36:45 -0300

    feat: add manifest and a new function to execute tests refactor: change the launchFragmentInContainer to launchFragmentInHiltContainer fix: android manifest activity theme

--- app/src/androidTest/java/com/pvsb/marvelapp/presentation/home/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/pvsb/marvelapp/presentation/home/CharactersFragmentTest.kt
@@ -1,7 +1,7 @@
 package com.pvsb.marvelapp.presentation.home
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.pvsb.marvelapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -18,7 +18,7 @@ class CharactersFragmentTest{
 
     @Before
     fun setUp(){
-        launchFragmentInContainer<CharactersFragment>()
+        launchFragmentInHiltContainer<CharactersFragment>()
     }
 
     @Test
